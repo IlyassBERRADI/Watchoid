4cb4c599dbd15a8cc2856668ea8b66710bce2eda
Ilyass Berradi <berradi.ilyass2@gmail.com>
2024-05-10 15:15:29 +0200

    Added two TCP connexions

--- app/src/main/java/com/example/watchoid/MainActivity.kt
+++ app/src/main/java/com/example/watchoid/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.watchoid
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.watchoid.ui.theme.WatchoidTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import java.net.InetSocketAddress
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +37,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val coroutineScope = rememberCoroutineScope()
+                    val coroutineScope2 = rememberCoroutineScope()
+                    var text by remember { mutableStateOf("Response will show here") }
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Text(text = text)
+                        Button(onClick = {
+                            coroutineScope.launch(IO) {
+                                var server = InetSocketAddress("www.google.fr", 80)
+                                text=TCPClientWeb.getResponse("GET / HTTP/1.1\r\nHost: www.google.fr\r\n\r\n", server)
+                            }
+                            /*coroutineScope.launch(IO) {
+                                var server = InetSocketAddress( 7777)
+                                text=TCPClient.getResponse("abc", server)
+                            }
+                            coroutineScope2.launch(IO){
+                                TCPServer(7777).launch()
+                            }*/
+                        }) {
+                            Text("Send")
+                        }
+                    }
+
                 }
             }
         }
--- app/src/main/java/com/example/watchoid/TCPClientWeb.kt
+++ app/src/main/java/com/example/watchoid/TCPClientWeb.kt
@@ -10,10 +10,11 @@ import java.nio.ByteBuffer
 import java.nio.channels.SocketChannel
 import java.nio.charset.Charset
 
-class TCPClient() {
+class TCPClientWeb() {
     companion object {
         private val UTF8_CHARSET: Charset = Charset.forName("UTF-8")
         private const val BUFFER_SIZE = 1024
+
 
         @Throws(IOException::class)
         fun getResponse(request : String, server : SocketAddress) : String {
@@ -22,23 +23,13 @@ class TCPClient() {
                 socketChannel = SocketChannel.open()
                 var bool = socketChannel.connect(server)
                 var requestBuffer = UTF8_CHARSET.encode(request)
-                var bufferInt = ByteBuffer.allocate(Int.SIZE_BYTES)
-                bufferInt.putInt(requestBuffer.remaining())
-                bufferInt.flip()
-                socketChannel.write(bufferInt)
                 var bytesWritten = socketChannel.write(requestBuffer)
-                bufferInt.clear()
-                if (!readFully(socketChannel, bufferInt)){
-                    Log.i("Server","Server stopped connection")
-                    return "";
-                }
-                bufferInt.flip()
-                var size = bufferInt.getInt()
-                var readBuffer = ByteBuffer.allocate(size)
-
-                if (!readFully(socketChannel, readBuffer)){
-                    Log.i("Error2","Client stopped connection")
-                    return "";
+                socketChannel.socket().shutdownOutput()
+                var readBuffer = ByteBuffer.allocate(BUFFER_SIZE)
+                while (socketChannel.read(readBuffer)!=-1){
+                    if (!readBuffer.hasRemaining()){
+                        break
+                    }
                 }
                 readBuffer.flip()
                 var charBuffer = UTF8_CHARSET.decode(readBuffer)
@@ -48,15 +39,6 @@ class TCPClient() {
             }
         }
 
-        @Throws(IOException::class)
-        private fun readFully(socketChannel : SocketChannel, buffer : ByteBuffer) : Boolean{
-            while (socketChannel.read(buffer)!=-1){
-                if (!buffer.hasRemaining()){
-                    return true
-                }
-            }
-            return  false
-        }
     }
 
 
